47fb6decc2210a40ae79fd03b37a7543416870ac
Kingshuk3006 <kingsarkar3006@gmail.com>
2025-12-05 11:46:24 +0530

    quotation can added without email

--- src/schemas/customer.schema.ts
+++ src/schemas/customer.schema.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const CustomerSchema = z.object({
   name: z.string().min(1, 'Customer name is required'),
-  email: z.email('Customer email must be valid').optional(),
+  email: z.string('Customer email must be valid').optional(),
   address: z.string().optional(),
   phoneNumber: z.string().optional(),
   VATNumber: z.string().optional(),
